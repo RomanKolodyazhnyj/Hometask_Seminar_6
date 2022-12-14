71f82ad10014d15f0f602ebd1fe862875a0d17fd
Roman <romek20@mail.ru>
2022-12-14 22:58:50 +0300

    Решение задачи 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,37 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.Write("Введите количество M: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+
+int[] array = PrintNumbers(numbers);
+
+int count = GetCount(array);
+Console.WriteLine($"Количество чисел больше нуля: {count}");
+
+int[] PrintNumbers(int numbers)
+{
+    int[] array = new int[numbers];
+    for (int i = 0; i < numbers; i++)
+    {
+        Console.Write($"Введите число {i+1}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int GetCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
